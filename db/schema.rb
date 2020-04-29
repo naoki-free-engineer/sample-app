1fc94e0daa056fbb93e600e0e56b1f147bb8fe18
EC2 Default User <ec2-user@ip-172-31-46-67.ap-northeast-1.compute.internal>
2020-04-29 17:16:58 +0000

    C1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190518043120) do
+ActiveRecord::Schema.define(version: 20200421043513) do
+
+  create_table "samples", force: :cascade do |t|
+    t.date "worked_on"
+    t.string "note"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_samples_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
